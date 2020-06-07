97053774434934ed0ebc7c605ef2666c51c3cc2e
Patrik Vámosi <patrikvamosi@Patrik-MacBook-Air.local>
2020-06-07 23:51:35 +0200

    dinamic components !!

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -2,10 +2,14 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <app-quote>
-          <h2 slot="title"> {{ quoteTitle }}</h2>
-          <p>A wonderful quote</p>
-        </app-quote>
+        <button @click="selectedComponent = 'appQuote'">Quote</button>
+        <button @click="selectedComponent = 'appAuthor'">Author</button>
+        <button @click="selectedComponent = 'appNew'">New</button>
+        <hr />
+        <p>{{ selectedComponent }}</p>
+        <component :is="selectedComponent">
+          <p>Default Content</p>
+        </component>
       </div>
     </div>
   </div>
@@ -13,14 +17,19 @@
 
 <script>
 import Qoute from "./components/Quote.vue";
+import Author from "./components/Author.vue";
+import New from "./components/New.vue";
 export default {
-  data: function(){
-    return{
-      quoteTitle: 'The Qoute'
-    }
+  data: function() {
+    return {
+      quoteTitle: "The Qoute",
+      selectedComponent: "appQuote"
+    };
   },
   components: {
-    appQuote: Qoute
+    appQuote: Qoute,
+    appAuthor: Author,
+    appNew: New
   }
 };
 </script>
