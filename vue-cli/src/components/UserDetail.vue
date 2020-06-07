84ea80a87d9292fd2c1b23bf9ce3586f34c2bbe2
Patrik Vámosi <patrikvamosi@Patrik-MacBook-Air.local>
2020-06-07 22:22:47 +0200

    component communication from child to parent

--- vue-cli/src/components/UserDetail.vue
+++ vue-cli/src/components/UserDetail.vue
@@ -3,22 +3,20 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User name: {{ switchName() }}</p>
+        <button @click="resetName"> Reset Name</button>
     </div>
 </template>
 
 <script>
 export default {
-    props: {
-        name: {
-            type:String,
-            default: function (){
-                return 'Max'
-            }
-        }
-    },
+    props: ['name'],
     methods: {
         switchName(){
             return this.name.split("").reverse().join("");
+        },
+        resetName(){
+            this.name = 'Max';
+            this.$emit('nameWasReset',this.name);
         }
     },
 }
