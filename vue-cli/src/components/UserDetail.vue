5748acaa5b19fc54b893339dea231da0a917f061
Patrik Vámosi <patrikvamosi@Patrik-MacBook-Air.local>
2020-06-07 22:01:39 +0200

    passing data to the child from the parent

---
+++ vue-cli/src/components/UserDetail.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User name: {{name}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name']
+}
+</script>
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
