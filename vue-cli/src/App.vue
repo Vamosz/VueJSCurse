f5cd06326a37ed460c658b4f4498c3ebabcebfa0
Patrik Vámosi <patrikvamosi@Patrik-MacBook-Air.local>
2020-06-09 22:03:07 +0200

    textare line break saving to use multi lienes

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -76,7 +76,7 @@
             <p>Mail: {{ userData.email }}</p>
             <p>Password: {{userData.password}}</p>
             <p>Age: {{userData.age}}</p>
-            <p>Message: {{message}}</p>
+            <p style="white-space: pre">Message: {{message}}</p>
             <p>
               <strong>Send Mail?</strong>
             </p>
