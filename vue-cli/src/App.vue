ba64738e8809ec758a42f0ded49a8ee4fe03c728
Patrik Vámosi <patrikvamosi@Patrik-MacBook-Air.local>
2020-06-09 22:08:10 +0200

    checkboxes in an array

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -32,10 +32,10 @@
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <div class="form-group">
             <label for="sendmail">
-              <input type="checkbox" id="sendmail" value="SendMail" /> Send Mail
+              <input type="checkbox" id="sendmail" value="SendMail" v-model="sendMail"/> Send Mail
             </label>
             <label for="sendInfomail">
-              <input type="checkbox" id="sendInfomail" value="SendInfoMail" /> Send Infomail
+              <input type="checkbox" id="sendInfomail" value="SendInfoMail" v-model="sendMail"/> Send Infomail
             </label>
           </div>
         </div>
@@ -81,7 +81,7 @@
               <strong>Send Mail?</strong>
             </p>
             <ul>
-              <li></li>
+              <li v-for="(item, index) in sendMail" :key="index"> {{item}}</li>
             </ul>
             <p>Gender:</p>
             <p>Priority:</p>
@@ -102,7 +102,8 @@ export default {
         password: "",
         age: 27
       },
-      message: 'A new Text'
+      message: 'A new Text',
+      sendMail: []
     };
   }
 };
