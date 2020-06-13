82371353c6b5e73128c3f3d084478c2453eeba24
Patrik Vámosi <patrikvamosi@Patrik-MacBook-Air.local>
2020-06-13 21:32:12 +0200

    Use mixins

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,47 +1,35 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Filters & Mixins</h1>
-                <p>{{ text | toUppercase | to-lowercase }}</p>
-                <hr>
-                <input v-model="filterText">
-                <ul>
-                    <li v-for="fruit in filteredFruits" :key="fruit">{{ fruit }}</li>
-                </ul>
-                <hr>
-                <app-list></app-list>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters & Mixins</h1>
+        <p>{{ text | toUppercase | to-lowercase }}</p>
+        <hr />
+        <input v-model="filterText" />
+        <ul>
+          <li v-for="fruit in filteredFruits" :key="fruit">{{ fruit }}</li>
+        </ul>
+        <hr />
+        <app-list></app-list>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import List from './List.vue'
-    export default {
-        data() {
-            return {
-                text: 'Hello there!',
-                fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
-                filterText: ''
-            }
-        },
-        filters: {
-            toUppercase(value) {
-                return value.toUpperCase();
-            }
-        },
-        computed: {
-            filteredFruits() {
-                return this.fruits.filter((element) => {
-                    return element.match(this.filterText);
-                });
-            }
-        },
-        components:{
-          appList: List
-        }
-    }
+import List from "./List.vue";
+import { fruitMixin } from "./FruitMixin";
+export default {
+  data() {
+    return {
+      text: "Hello there!"
+    };
+  },
+  mixins: [fruitMixin],
+  components: {
+    appList: List
+  }
+};
 </script>
 
 <style>
