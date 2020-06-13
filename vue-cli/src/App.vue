f8a1bd3318443c1df66afff81d29bfe575605ca5
Patrik Vámosi <patrikvamosi@Patrik-MacBook-Air.local>
2020-06-13 12:07:11 +0200

    more complex directives

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -13,7 +13,7 @@
         <h1>Custom Directives</h1>
         <p v-highlight:background.delayed="'red'">Color this</p>
         <p v-highlight="'red'">Color this</p>
-        <p v-local-highlight:background.delayed.blink="'red'">Color this</p>
+        <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500 }">Color this</p>
       </div>
     </div>
   </div>
@@ -29,8 +29,8 @@ export default {
           delay = 3000;
         }
         if (binding.modifiers["blink"]) {
-          let mainColor = binding.value;
-          let secondColor = "blue";
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
           let currentColor = mainColor;
           setTimeout(() => {
             setInterval(() => {
@@ -41,14 +41,14 @@ export default {
               } else {
                 el.style.color = currentColor;
               }
-            }, 1000);
+            }, binding.value.delay);
           }, delay);
         } else {
           setTimeout(() => {
             if (binding.arg == "background") {
-              el.style.background = binding.value;
+              el.style.background = binding.value.mainColor;
             } else {
-              el.style.color = binding.value;
+              el.style.color = binding.value.mainColor;
             }
           }, delay);
         }
