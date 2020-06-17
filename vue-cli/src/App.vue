101651b38e6afc6e416554ac5fcd079f41b74b9a
Vamosz <vamosipatrik@gmail.com>
2020-06-17 21:40:44 +0200

    javascript events for animation

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -32,7 +32,19 @@
           <div class="alert alert-info" v-if="show" key="info">Some info</div>
           <div class="alert alert-warning" v-else key="warning">Some warning</div>
         </transition>
-
+        <hr>
+        <button class="btn btn-primary" @click="load = !load">Load / remove Element</button>
+        <br><br>
+        <transition @before-enter="beforeEnter"
+                    @enter="enter"
+                    @after-enter="afterEnter"
+                    @enter-cancelled="enterCancelled"
+                    @before-leave="beforeLeave"
+                    @leave="leave"
+                    @after-leave="afterLeave"
+                    @leave-cancelled="leaveCancelled">
+          <div style="width: 100px; height: 100px; background: lightgreen" v-if="load"></div>
+        </transition>
       </div>
     </div>
   </div>
@@ -42,10 +54,41 @@
   export default {
     data() {
       return {
-        show: true,
+        show: false,
+        load: true,
         alertAnimation: 'fade'
       }
+    },
+    methods: {
+      beforeEnter(el) {
+        console.log('beforeEnter');
+      },
+      enter(el, done) {
+        console.log('enter');
+        done();
+      },
+      afterEnter(el) {
+        console.log('afterEnter');
+      },
+      enterCancelled(el) {
+        console.log('entercanc');
+      },
+      beforeLeave(el) {
+        console.log('beforeLeave');
+      },
+      leave(el, done){
+        console.log('leave');
+        done();
+      },
+      afterLeave(el){
+        console.log('afterLeave');
+      },
+      leaveCancelled(el){
+        console.log('leaveCancelled');
+      }
+
     }
+
   }
 </script>
 
