0c65ed86b8362ab8b68557d109c1809e1e68b9fa
Vamosz <vamosipatrik@gmail.com>
2020-06-17 20:43:59 +0200

    First animation

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -4,7 +4,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
         <hr>
-        <button class="btn btn-primary" @click="show = !show"> Show ALert</button>
+        <button class="btn btn-primary" @click="show = !show"> Show Alert</button>
         <br>
         <br>
         <transition name="fade">
@@ -28,18 +28,20 @@
 
 <style>
   .fade-enter {
-
+    opacity: 0;
   }
 
   .fade-enter-active {
-
+    transition: opacity 1s;
+    /*opacity: 1; default dont use*/
   }
 
   .fade-leave {
-
+    /*opacity: 1;*/
   }
 
   .fade-leave-active {
-
+    transition: opacity 1s;
+    opacity: 0;
   }
 </style>
