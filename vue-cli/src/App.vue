79434df150f3008443d1424f0ac6274ec05eef6e
Vamosz <vamosipatrik@gmail.com>
2020-06-17 20:28:48 +0200

    start code fix

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,23 +1,24 @@
 <template>
-  <div id="app">
-    <app-header />
-    <router-view></router-view>
-  </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Animations</h1>
+
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-  import Header from './components/header/header.vue'
-  export default {
-    name: 'app',
-    components: {
-      'app-header': Header
+    export default {
+        data() {
+            return {
+
+            }
+        }
     }
-  }
 </script>
 
 <style>
-  body, html {
-    margin: 0;
-    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-  }
+
 </style>
