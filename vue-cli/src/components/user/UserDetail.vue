094022ecd28a2a0bfad00bb1a9cf5c720a664a63
Vámosi Patrik <vamosipatrik@gmail.com>
2020-07-21 22:49:39 +0200

    Using routerlink to go to the edit page

--- vue-cli/src/components/user/UserDetail.vue
+++ vue-cli/src/components/user/UserDetail.vue
@@ -3,6 +3,7 @@
     <div>
         <h3>Some User Details</h3>
         <p>User loaded has ID: {{this.$route.params.id}}</p>
+        <router-link tag="button" :to="'/user/' + this.$route.params.id+ '/edit'" class="btn btn-primary">Edit User</router-link>
     </div>
 </template>
 <script>
