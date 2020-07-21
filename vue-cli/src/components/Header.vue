99ef32ccdd64b379a667d2395ab9513357708fac
Vámosi Patrik <vamosipatrik@gmail.com>
2020-07-21 22:47:16 +0200

    Using child routes

--- vue-cli/src/components/Header.vue
+++ vue-cli/src/components/Header.vue
@@ -2,9 +2,7 @@
     <ul class="nav nav-tabs" id="myTab" role="tablist">
         <a  class="nav-item"><router-link to="/" tag="li" class="nav-link" role="presentation" active-class="active" exact>Home
         </router-link></a>
-        <a class="nav-item" ><router-link to="/user/2" tag="li" class="nav-link" role="presentation" active-class="active">User 2
-        </router-link></a>
-        <a class="nav-item" ><router-link to="/user/1" tag="li" class="nav-link" role="presentation" active-class="active">User 1
+        <a class="nav-item" ><router-link to="/user" tag="li" class="nav-link" role="presentation" active-class="active">User
         </router-link></a>
     </ul>
 </template>
--- vue-cli/src/components/user/UserDetail.vue
+++ vue-cli/src/components/user/UserDetail.vue
@@ -1,3 +1,10 @@
 <template>
-    <h3>Some User Details</h3>
+
+    <div>
+        <h3>Some User Details</h3>
+        <p>User loaded has ID: {{this.$route.params.id}}</p>
+    </div>
 </template>
+<script>
+
+</script>
