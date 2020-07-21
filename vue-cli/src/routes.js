80c521e87749f8a13ba52784556eca903a0f4f20
Vámosi Patrik <vamosipatrik@gmail.com>
2020-07-21 23:57:38 +0200

    Using lazy loading and "lazy" groups

--- vue-cli/src/routes.js
+++ vue-cli/src/routes.js
@@ -1,9 +1,29 @@
-import User from './components/user/User'
-import UserStart from './components/user/UserStart'
-import UserDetail from './components/user/UserDetail'
-import UserEdit from './components/user/UserEdit'
 import Home from './components/Home'
 import Header from './components/Header'
+
+const User = resolve => {
+    require.ensure(['./components/user/User'], () => {
+        resolve(require('./components/user/User'));
+    }, 'user')
+}
+
+const UserStart = resolve => {
+    require.ensure(['./components/user/UserStart'], () => {
+        resolve(require('./components/user/UserStart'));
+    }, 'user')
+}
+
+const UserEdit = resolve => {
+    require.ensure(['./components/user/UserEdit'], () => {
+        resolve(require('./components/user/UserEdit'));
+    })
+}
+const UserDetail = resolve => {
+    require.ensure(['./components/user/UserDetail'], () => {
+        resolve(require('./components/user/UserDetail'));
+    })
+}
+
 
 export const routes = [
     {
