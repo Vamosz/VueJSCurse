3681cb0db3e965d34cfcd35b458a83312aea19ad
Vámosi Patrik <vamosipatrik@gmail.com>
2020-07-21 22:23:32 +0200

    Creating navigation and style it

---
+++ vue-cli/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <a  class="nav-item"><router-link to="/" tag="li" class="nav-link" role="presentation" active-class="active" exact>Home
+        </router-link></a>
+        <a class="nav-item" ><router-link to="/user" tag="li" class="nav-link" role="presentation" active-class="active">User
+        </router-link></a>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -4,10 +4,12 @@ import VueRouter from 'vue-router'
 import {routes} from './routes'
 
 Vue.use(VueRouter);
+
 const router = new VueRouter({
   routes,
   mode: 'history'
 })
+
 new Vue({
   el: '#app',
   router,
