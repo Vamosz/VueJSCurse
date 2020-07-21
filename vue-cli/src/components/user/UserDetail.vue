4689d2b57e99523ac22307e6128bb547bdf888f5
Vámosi Patrik <vamosipatrik@gmail.com>
2020-07-21 23:02:26 +0200

    Using query params.

--- vue-cli/src/components/user/UserDetail.vue
+++ vue-cli/src/components/user/UserDetail.vue
@@ -3,7 +3,9 @@
     <div>
         <h3>Some User Details</h3>
         <p>User loaded has ID: {{this.$route.params.id}}</p>
-        <router-link tag="button" :to="{ name: 'userEdit', params:{ id: this.$route.params.id } }" class="btn btn-primary">Edit User</router-link>
+        <router-link tag="button"
+                     :to="{ name: 'userEdit', params:{ id: this.$route.params.id }, query:{locale: 'en', q: 100} }"
+                     class="btn btn-primary">Edit User</router-link>
     </div>
 </template>
 <script>
