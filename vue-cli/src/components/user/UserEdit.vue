ec0234f6f1763dd287c61e442e0a15ceba95f35e
Vámosi Patrik <vamosipatrik@gmail.com>
2020-07-21 23:49:18 +0200

    before leave the page..

--- vue-cli/src/components/user/UserEdit.vue
+++ vue-cli/src/components/user/UserEdit.vue
@@ -7,3 +7,23 @@
     </div>
 
 </template>
+<script>
+    export default {
+        data(){
+            return {
+                confirmed: false
+            }
+        },
+        beforeRouteLeave(to,from,next){
+            if(this.confirmed){
+                next();
+            }else{
+                if(confirm('sure?')){
+                    next();
+                }else{
+                    next(false);
+                }
+            }
+        }
+    }
+</script>
