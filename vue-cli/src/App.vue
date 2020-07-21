dd39140ebfef9d5f0388ef8dceab66e8aa24f9ff
Vámosi Patrik <vamosipatrik@gmail.com>
2020-07-21 22:27:31 +0200

    Button to navigate home or somewhere(navigation in script)

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <app-header></app-header>
