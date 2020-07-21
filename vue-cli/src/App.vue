78fb73dd186de57f08b327dbf9c7ef167d6563b1
Vámosi Patrik <vamosipatrik@gmail.com>
2020-07-21 23:09:06 +0200

    Multiple router vue

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -2,18 +2,19 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <app-header></app-header>
+                <router-view name="header-top"></router-view>
                 <router-view></router-view>
+                <router-view name="header-bottom"></router-view>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Header from "./components/Header";
+    // import Header from "./components/Header";
     export default {
         components:{
-            appHeader: Header
+            // appHeader: Header
         }
     }
 </script>
--- vue-cli/src/routes.js
+++ vue-cli/src/routes.js
@@ -2,11 +2,21 @@ import User from './components/user/User'
 import UserStart from './components/user/UserStart'
 import UserDetail from './components/user/UserDetail'
 import UserEdit from './components/user/UserEdit'
-import Home from './components/Home.vue'
+import Home from './components/Home'
+import Header from './components/Header'
 
 export const routes = [
-    {path: '/', component: Home},
-    {path: '/user', component: User,
+    {
+        path: '', name: 'home', components: {
+            default: Home,
+            'header-top': Header,
+        }
+    },
+    {
+        path: '/user', components: {
+            default: User,
+            'header-bottom': Header
+        },
         children: [
             {path: '', component: UserStart},
             {path: ':id', component: UserDetail},
