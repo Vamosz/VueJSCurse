5da322f28adcd0fca3e73f560dd3e4e9663328ef
Vámosi Patrik <vamosipatrik@gmail.com>
2020-07-22 19:02:16 +0200

    Using separate files for vuex

--- vue-cli/src/store/store.js
+++ vue-cli/src/store/store.js
@@ -1,45 +1,24 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import counter from "./modules/counter";
+import * as actions from "./actions"
+import * as mutations from "./mutations"
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        counter: 0
+        value: 0
     },
     getters: {
-        doubleCounter: state => {
-            return state.counter * 2;
-        },
-        stringCounter: state => {
-            return state.counter + " Clicks";
+        value: state => {
+            return state.value
         }
     },
     //mutations is a sync method so you cant use in async! for that you should use actions!
-    mutations: {
-        increment: (state, payload) => {
-            state.counter += payload;
-        },
-        decrement: (state, payload) => {
-            state.counter -= payload;
-        }
-    },
-    actions: {
-        increment: ({commit}, payload) => {
-            commit("increment", payload);
-        },
-        decrement: ({commit}) => {
-            commit("decrement");
-        },
-        asyncIncrement: ({commit}, payload) => {
-            setTimeout(() => {
-                commit("increment", payload.by);
-            }, payload.duration);
-        },
-        asyncDecrement: ({commit}, payload) => {
-            setTimeout(() => {
-                commit("decrement", payload.by);
-            }, payload.duration);
-        }
+    mutations,
+    actions,
+    modules:{
+        counter
     }
 });
