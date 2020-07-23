01670010d8e8c28a795132d475886dd140b70bd6
Vámosi Patrik <vamosipatrik@gmail.com>
2020-07-23 13:30:50 +0200

    Storing the user

--- vue-cli/src/store.js
+++ vue-cli/src/store.js
@@ -5,36 +5,47 @@ import axios from "./axios-auth";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    idToken: null,
-    userId: null
-  },
-  mutations: {
-
-  },
-  actions: {
-    signup( {commit}, authData ){
-      commit;
-      axios.post('/accounts:signUp?key=AIzaSyDsaqGiLt9e0-VA6S3GsknFkkM8iiqU99A', {
-        email: authData.email,
-        password: authData.password,
-        returnSecureToken: true
-      })
-          .then(res => console.log(res))
-          .catch(error => console.log(error))
+    state: {
+        idToken: null,
+        userId: null
     },
-    login({commit},authData){
-      commit;
-      axios.post('/accounts:signInWithPassword?key=AIzaSyDsaqGiLt9e0-VA6S3GsknFkkM8iiqU99A', {
-        email: authData.email,
-        password: authData.password,
-        returnSecureToken: true
-      })
-          .then(res => console.log(res))
-          .catch(error => console.log(error))
-    }
-  },
-  getters: {
-
-  }
+    mutations: {
+        authUser(state, userData) {
+            state.idToken = userData.token;
+            state.userId = userData.userId;
+        }
+    },
+    actions: {
+        signup({commit}, authData) {
+            axios.post('/accounts:signUp?key=AIzaSyDsaqGiLt9e0-VA6S3GsknFkkM8iiqU99A', {
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            })
+                .then(res => {
+                    console.log(res);
+                    commit('authUser', {
+                        token: res.data.idToken,
+                        userId: res.data.localId
+                    });
+                })
+                .catch(error => console.log(error))
+        },
+        login({commit}, authData) {
+            axios.post('/accounts:signInWithPassword?key=AIzaSyDsaqGiLt9e0-VA6S3GsknFkkM8iiqU99A', {
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            })
+                .then(res => {
+                    console.log(res);
+                    commit('authUser', {
+                        token: res.data.idToken,
+                        userId: res.data.localId
+                    });
+                })
+                .catch(error => console.log(error))
+        }
+    },
+    getters: {}
 });
