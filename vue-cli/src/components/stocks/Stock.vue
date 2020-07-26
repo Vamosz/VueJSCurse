e4b2a7ca59d4f49dc46d135addadd0c78f61799a
Vámosi Patrik <vamosipatrik@gmail.com>
2020-07-26 13:13:13 +0200

    Created stocks page and the stocks

--- vue-cli/src/components/stocks/Stock.vue
+++ vue-cli/src/components/stocks/Stock.vue
@@ -1,5 +1,16 @@
 <template>
-    <h1>The Stocks - Stock Component</h1>
+    <b-col col sm="6" md="4">
+        <b-card border-variant="success" header="Success (Price: PRICE)" align="center">
+            <b-card-text>
+                <b-row>
+                    <b-col col sm="6"> <input type="number" class="form-control"></b-col>
+                    <b-col col sm="6" class="text-right"><b-button variant="outline-success">Buy</b-button></b-col>
+                </b-row>
+
+
+            </b-card-text>
+        </b-card>
+    </b-col>
 </template>
 
 <script>
--- vue-cli/src/components/stocks/Stocks.vue
+++ vue-cli/src/components/stocks/Stocks.vue
@@ -1,10 +1,27 @@
 <template>
-    <h1>The Stocks Component</h1>
+    <b-row >
+        <app-stock class="mb-4" v-for="stock in stocks" :key="stock"></app-stock>
+    </b-row>
 </template>
 
 <script>
+    import Stock from "./Stock";
     export default {
-        name: "Stocks"
+        name: "Stocks",
+        data(){
+            return {
+                stocks:[
+                    {id: 1, name:'BMW', price: 110},
+                    {id: 1, name:'Google', price: 200},
+                    {id: 1, name:'Apple', price: 250},
+                    {id: 1, name:'Twitter', price: 80}
+                ]
+            }
+        },
+        components:{
+            appStock: Stock
+        }
+
     }
 </script>
 
