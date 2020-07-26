3d1c5c4731bb2293bd599752da22fc76a9bc0d5d
Vámosi Patrik <vamosipatrik@gmail.com>
2020-07-26 13:45:49 +0200

    Vuex store

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from "vue-router";
 import App from './App.vue'
 import {routes} from "./routes";
+import store from "./store/store";
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -19,5 +20,6 @@ const router = new VueRouter({
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
